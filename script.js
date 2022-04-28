5e7fd618641dd1ac582f608b8c936826cc2ede46
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-04-28 11:57:26 -0300

    req02 e 03

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 // const { fetchItem } = require("./helpers/fetchItem");
 
+// const { fetchItem } = require("./helpers/fetchItem");
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -16,6 +18,25 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function cartItemClickListener(event) {
+  event.target.remove();
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const GetIdProduct = async (event) => {
+  const idProduct = await fetchItem(event.target.parentNode.firstChild.innerText);
+  const cart = document.querySelector('.cart__items');
+  const { id, title, price } = idProduct;
+  cart.appendChild(createCartItemElement({ id, title, price }));
+};
+
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -23,25 +44,14 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  .addEventListener('click', GetIdProduct);
   return section;
 }
 
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  event.target.remove();
-}
-
-function createCartItemElement({ id: sku, title: name, thumbnail: salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 const AppendItens = async () => { // adiciona elementos da api a pagina
